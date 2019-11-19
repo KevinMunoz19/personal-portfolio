a3849f4e8da1c8869b448bcb0f13ad710313d0ea
Kevin Estuardo Muñoz Muñoz <mun15358@uvg.edu.gt>
2019-11-18 18:56:14 -0600

    Add files via upload

---
+++ precache-manifest.0d1aadd3b0e7fdb4609d9be83a4384a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb4404ffcb449424cc1e0845f7646de",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "34b87f939133df379734",
+    "url": "/personal-portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "ce521bda9066b6a6a307",
+    "url": "/personal-portfolio/static/css/main.4362f2c0.chunk.css"
+  },
+  {
+    "revision": "34b87f939133df379734",
+    "url": "/personal-portfolio/static/js/2.955fa404.chunk.js"
+  },
+  {
+    "revision": "ce521bda9066b6a6a307",
+    "url": "/personal-portfolio/static/js/main.1750ecbe.chunk.js"
+  },
+  {
+    "revision": "95008f69b66ca0350046",
+    "url": "/personal-portfolio/static/js/runtime-main.61288b49.js"
+  }
+]);
